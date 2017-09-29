c275f373cbf11dae6523580c0e760a51dc995920
Flavio Luiz Cruz <flcable@gmail.com>
2017-09-29 01:14:47 -0300

    Cap1

---
+++ Memberships/Entities/UserSubscription.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Entities
+{
+    [Table("UserSubscription")]
+    public class UserSubscription
+    {
+        [Required]
+        [Key, Column(Order = 1)]
+        public int SubscriptionId { get; set; }
+
+        
+        [Key, Column(Order = 2)]
+        [MaxLength(128)]
+        [Required]
+        public string UserID { get; set; }
+
+
+       public DateTime? StartDate { get; set; }
+        public DateTime? EndtDate { get; set; }
+
+
+    }
+}
